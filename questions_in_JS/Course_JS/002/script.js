06cdb6bb41922a22676af2d1eaeb0fe4fdce0b7b
Junior Silva <junior.silva@ccc.ufcg.edu.br>
2022-04-16 20:18:01 -0300

    Basics functions of array

--- questions_in_JS/Course_JS/002/script.js
+++ questions_in_JS/Course_JS/002/script.js
@@ -53,7 +53,6 @@ function fruitProcessor(fruitA, fruitB){
 }
 
 console.log(fruitProcessor(3,9));
-*/
  
 //44,23,71,65,54,49
 //85,54,41,23,34,27
@@ -83,3 +82,35 @@ function checkwinner(avgDolphins, avgKoalas) {
 console.log(checkwinner(scoreDolphins,scoreKoalas));
 
 
+// Basic Array Operations (Methods)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
+*/
